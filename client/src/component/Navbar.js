e506f71dd991224302fde737c73442feae39db23
unknown <shivamrana.ca@gmail.com>
2023-01-20 19:56:17 -0500

    cahge the padding in navbar componet

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () =>{
         <div className = "navbar" style={{display:"flex" , margin:"60px 130px 0 130px" }}>
             {
                 navData.map(data => (
-                    <div className = "navbarContent" style={{padding:"0 20px "}}>
+                    <div className = "navbarContent" style={{padding:"0 27px "}}>
                         <img src={data.url}  alt="nav" style={{width:"50px", height:'50px'}}/>
                         <p style={{width:"70%",textAlign:"center"}}>{data.text}</p>
                     </div> 
